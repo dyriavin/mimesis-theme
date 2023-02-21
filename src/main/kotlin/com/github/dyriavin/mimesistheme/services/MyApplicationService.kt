32652a940b08ff880c347e94cde273a643ed26f4
GitHub Action <action@github.com>
2023-02-21 00:52:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/dyriavin/mimesistheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dyriavin/mimesistheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dyriavin.mimesistheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dyriavin.mimesistheme.MyBundle
 
 class MyApplicationService {
 
